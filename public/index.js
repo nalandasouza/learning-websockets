cc00ac066a3f948e0cd679baca16bdab30743e16
Nalanda Souza <nalanda.ue@gmail.com>
2025-09-21 14:21:44 -0300

    improving

--- public/index.js
+++ public/index.js
@@ -3,12 +3,15 @@ import { emitirAdicionarDocumento } from "./socket-front-index.js";
 const listaDocumentos = document.getElementById("lista-documentos");
 const form = document.getElementById("form-adiciona-documento");
 const inputDocumento = document.getElementById("input-documento");
+const btnLogout = document.getElementById("botao-logout");
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
   emitirAdicionarDocumento(inputDocumento.value);
   inputDocumento.value = "";
 });
+
+btnLogout.addEventListener("click", () => window.location.href = "/login");
 
 function inserirLinkDocumento(nomeDocumento) {
   listaDocumentos.innerHTML += `
